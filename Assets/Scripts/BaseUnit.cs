0b035bff23fb19f73c1b9355b33c0f14f97b7ce6
Vladimir Kanaev <v.kanaev2112@gmail.com>
2015-01-22 01:23:44 +0300

    Added BaseUnit class for all units

---
+++ Assets/Scripts/BaseUnit.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseUnit : MonoBehaviour {
+    private int HealthPoints;
+    private int ArmorPoints;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
